75a67ca9feadc9ba62d1a353c3580129575abc15
Surajkumble02 <93071978+Surajkumble02@users.noreply.github.com>
2026-09-01 17:51:53 +0530

    api-test.sh - Test Commit 1

--- api-test.sh
+++ api-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-API_URL="https://main.manage.atco-train.suite.maximo.com/maximo/api/dm/distribute?apikey=vn10ngdbhqbt1utvinmtppp5g3fdbd0chm466mj5"
+API_URL="https://main.manage.atco-train.suite.maximo.com/maximo/api/dm/create?apikey=vn10ngdbhqbt1utvinmtppp5g3fdbd0chm466mj5"
 
 status_code=$(curl \
   -o response.json \
